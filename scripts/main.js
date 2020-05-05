47f7a53ceb3a6e7760d224791962682559584603
ryanowalls <57993756+ryanowalls@users.noreply.github.com>
2020-05-05 15:39:20 +0100

    Fix 100vh for mobile

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,9 @@
 var logo = document.getElementById('logo-wrapper')
 
 function ready() {
-	// animateLogo()
+
+	setVH()
+
 	animateHeading()
 }
 
@@ -43,3 +45,15 @@ function animateLogo() {
 function show(el) {
 	el.style.visibility = 'visible'
 }
+
+function setVH() {
+	// first we get the viewport height and we multiple it by 1% to get a value for a vh unit
+	let vh = window.innerHeight * 0.01
+	// then we set the value in the --vh custom property to the root of the document
+	document.documentElement.style.setProperty('--vh', `${vh}px`)
+}
+
+window.addEventListener('resize', () => {
+	let vh = window.innerHeight * 0.01;
+	document.documentElement.style.setProperty('--vh', `${vh}px`);
+})
