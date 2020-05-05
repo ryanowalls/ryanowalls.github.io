060d867b626b934a7b098b140d0a13fbd2b8e95e
ryanowalls <57993756+ryanowalls@users.noreply.github.com>
2020-05-05 17:19:07 +0100

    Updated heading animation

--- scripts/main.js
+++ scripts/main.js
@@ -4,24 +4,53 @@ function ready() {
 
 	setVH()
 
+	overlapHeadings()
+
 	animateHeading()
 }
 
 function animateHeading() {
 	// Wrap every letter in a span
-	var textWrapper = document.querySelector('.heading .letters');
-	textWrapper.innerHTML = textWrapper.textContent.replace(/([^\x00-\x80]|\w)/g, "<span class='letter'>$&</span>");
+	var textWrapper1 = document.querySelector('.heading .letters1')
+	textWrapper1.innerHTML = textWrapper1.textContent.replace(/([^\x00-\x80]|\w)/g, "<span class='letter'>$&</span>")
+	var textWrapper2 = document.querySelector('.heading .letters2')
+	textWrapper2.innerHTML = textWrapper2.textContent.replace(/([^\x00-\x80]|\w)/g, "<span class='letter'>$&</span>")
+
+	show(textWrapper1)
+	show(textWrapper2)
 
 	anime.timeline({
-		// loop: true
+		loop: true
 	})
 	.add({
-		targets: '.heading .letter',
+		targets: '.letters1 .letter',
+		opacity: [0,1],
+		easing: "easeOutExpo",
+		duration: 500,
+		delay: anime.stagger(25, {start: 0})
+	})
+	.add({
+		targets: '.letters1 .letter',
+		opacity: [1,0],
+		easing: "easeOutExpo",
+		duration: 500,
+		delay: anime.stagger(25, {start: 1000})
+	})
+	.add({
+		targets: '.letters2 .letter',
 		opacity: [0,1],
 		easing: "easeOutExpo",
 		duration: 500,
 		offset: '-=775',
-		delay: (el, i) => 34 * (i+1)
+		delay: anime.stagger(25, {start: 0})
+	}, '-=250')
+	.add({
+		targets: '.letters2 .letter',
+		opacity: [1,0],
+		easing: "easeOutExpo",
+		duration: 500,
+		offset: '-=775',
+		delay: anime.stagger(25, {start: 1000})
 	})
 }
 
@@ -46,6 +75,13 @@ function show(el) {
 	el.style.visibility = 'visible'
 }
 
+function overlapHeadings() {
+	// overlap 2 headings
+	var heading2 = document.querySelector('.letters2')
+	var heading2height = heading2.getBoundingClientRect().height
+	heading2.style.marginTop = '-' + heading2height + 'px'
+}
+
 function setVH() {
 	// first we get the viewport height and we multiple it by 1% to get a value for a vh unit
 	let vh = window.innerHeight * 0.01
@@ -54,6 +90,7 @@ function setVH() {
 }
 
 window.addEventListener('resize', () => {
-	let vh = window.innerHeight * 0.01;
-	document.documentElement.style.setProperty('--vh', `${vh}px`);
+	let vh = window.innerHeight * 0.01
+	document.documentElement.style.setProperty('--vh', `${vh}px`)
+	overlapHeadings()
 })
